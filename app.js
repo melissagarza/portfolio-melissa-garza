e290b07f763b79ac7da403595ebbc228b97f255b
Melissa Garza <melissa.garza@utexas.edu>
2020-03-11 21:36:03 -0500

    Setup dotenv, connect database, and separate out home routes

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const routerHome = require('./routes/home');
+app.use('/', routerHome);
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Successfully connected to database.');
 });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));

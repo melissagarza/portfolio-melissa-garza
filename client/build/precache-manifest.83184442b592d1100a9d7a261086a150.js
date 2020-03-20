4d81066d2115ac3bb90771be98ffdbbf3c4d13cf
Melissa Garza <melissa.garza@utexas.edu>
2020-03-19 20:43:48 -0500

    Run build script and include hompage in package.json

---
+++ client/build/precache-manifest.83184442b592d1100a9d7a261086a150.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e81849c8ba6766ef5ca0f114b1700704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfa705389dfa42bdef94",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "edcf1631f4be3177e9a8",
+    "url": "/static/js/2.048ebf5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.048ebf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfa705389dfa42bdef94",
+    "url": "/static/js/main.d5a4de8b.chunk.js"
+  },
+  {
+    "revision": "2ea85aa4f106c6955dfa",
+    "url": "/static/js/runtime-main.e658e398.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

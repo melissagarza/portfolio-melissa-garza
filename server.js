91309acca23fb0a6ff0d24a64b5e148e8d4e4a05
Melissa Garza <melissa.garza@utexas.edu>
2020-03-20 02:11:22 -0500

    Restructure project by elevating api files to root

--- server.js
+++ server.js
@@ -23,11 +23,9 @@ app.use(express.json());
 app.use('/api', routerApi);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../../client/build'));
+  app.use(express.static('client/build'));
   app.get('*', (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, '../../client', 'build', 'index.html')
-    );
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 

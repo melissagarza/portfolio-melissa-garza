a1f88bd5680f29ec5b636bdbaaf10e1858f83b77
Melissa Garza <melissa.garza@utexas.edu>
2020-03-24 14:17:01 -0500

    Add the new keyword when creating a new mongoose schema

--- models/model.exercise.js
+++ models/model.exercise.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const ExerciseSchema = mongoose.Schema({
+const ExerciseSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',

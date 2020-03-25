718279aa2e3a9d0cd40cce5f078648ebe61945cb
Melissa Garza <melissa.garza@utexas.edu>
2020-03-25 15:57:13 -0500

    Add endpoint to return user information after being sent valid token in request header

--- routers/router.auth.js
+++ routers/router.auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { check } = require('express-validator');
+const auth = require('../middleware/auth');
 const { controllerAuth } = require('../controllers');
 
 const routerAuth = express.Router();
 
+routerAuth.get('/', auth, controllerAuth.getAuthenticatedUser);
 routerAuth.post('/', [
   check('email', 'A valid email is required').isEmail(),
   check('password', 'Password is required').exists()

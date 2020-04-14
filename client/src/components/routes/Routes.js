92440c2c912cb67684006fa59df12a1dd9cc4bf4
Melissa Garza <melissa.garza@utexas.edu>
2020-04-13 22:09:55 -0500

    Rename component since it doesn't relate to any single workout

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Exercises from '../workout/Exercises';
+import Exercises from '../exercise/Exercises';
 
 const Routes = () => (
   <section className="container">

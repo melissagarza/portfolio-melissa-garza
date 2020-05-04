93270fd5d222bc67321d23c4d94ae72814ed819d
Melissa Garza <melissa.garza@utexas.edu>
2020-05-04 15:49:53 -0500

    Add another chart with filtered exercises showing the authenticated users exercises if they have any

--- client/src/components/exercise/ExerciseChart.js
+++ client/src/components/exercise/ExerciseChart.js
@@ -1,30 +1,47 @@
-import React, { useEffect } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Loading from '../layout/Loading';
 import { createChart } from '../../utils/chart';
 
-const ExerciseChart = ({ exercise: { exercise, exercises, loading } }) => {
+const ExerciseChart = ({
+  auth: {
+    isAuthenticated,
+    user
+  },
+  exercise: {
+    exercise,
+    exercises,
+    loading
+  }}) => {
 
   useEffect(() => {
     if (!loading) {
       let chart = createChart('public', exercises);
+      let exercisesUser = exercises.filter(exercise => exercise.user.alias === user.alias);
+      let chartUser = createChart('user', exercisesUser);
+
       chart.draw();
+      chartUser.draw();
     }
-  }, [loading, exercises]);
+  }, [loading, exercises, user]);
 
   return loading ? (
     <Loading />
   ) : (
-    <div className="container-public"></div>
+    <Fragment>
+      {isAuthenticated && (<div className="container-user"></div>)}
+      <div className="container-public"></div>
+    </Fragment>
   );
 };
 
 ExerciseChart.propTypes = {
+  auth: PropTypes.object.isRequired,
   exercise: PropTypes.object,
   exercises: PropTypes.object
 };
 
-const mapStateToProps = ({ exercise }) => ({ exercise });
+const mapStateToProps = ({ auth, exercise }) => ({ auth, exercise });
 
 export default connect(mapStateToProps, {})(ExerciseChart);

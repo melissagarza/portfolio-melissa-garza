071df4ea5b70b0bd0d74f1ed545b0028ab91f35f
Melissa Garza <melissa.garza@utexas.edu>
2020-08-05 15:45:12 -0500

    Set up a component for Roadmap Bubble Chart

---
+++ client/src/components/roadmap/RoadmapChart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const RoadmapChart = () => {
+  return (
+    <div>
+      Roadmap Chart
+    </div>
+  );
+};
+
+export default connect()(RoadmapChart);
--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -4,6 +4,7 @@ import Register from '../auth/Register';
 import Login from '../auth/Login';
 import Resume from '../layout/Resume';
 import Exercise from '../exercise/Exercise';
+import Roadmap from '../roadmap/Roadmap';
 
 const Routes = () => (
   <Switch>
@@ -11,6 +12,7 @@ const Routes = () => (
     <Route exact path="/login" component={Login} />
     <Route exact path="/resume" component={Resume} />
     <Route exact path="/exercises" component={Exercise} />
+    <Route exact path="/roadmap" component={Roadmap} />
   </Switch>
 );
 

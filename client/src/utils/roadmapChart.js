5dc6f62b7690fd2691c301f123bbef49f6059f4e
Melissa Garza <melissa.garza@utexas.edu>
2020-08-11 17:54:26 -0500

    Set the CX and CY of the added ellipses based on the data's date duration and effort

--- client/src/utils/roadmapChart.js
+++ client/src/utils/roadmapChart.js
@@ -15,7 +15,17 @@ export const createRoadmapChart = () => {
   const widthChart = widthSvg - margin.left - margin.right;
   const heightChart = heightSvg - margin.top - margin.bottom;
 
-  const draw = (data) => {
+  const getDuration = (start, end) => {
+    return moment.duration(moment(end).diff(moment(start)));
+  };
+
+  const getHalfDuration = (start, end) => {
+    const dur = moment.duration(moment(end).diff(moment(start)));
+    const halfMs = dur.asMilliseconds() / 2;
+    return dur.subtract(halfMs, 'milliseconds');
+  };
+
+  const draw = (data = []) => {
 
     if (data.length < 0) {
       return;
@@ -29,12 +39,15 @@ export const createRoadmapChart = () => {
 
     allStartEndDates = allStartEndDates.sort((a, b) => a.diff(b));
 
+    const allEfforts = _.map(data, d => d.effort / d.manpower);
+    const maxEffort = d3.max(allEfforts);
+
     const scaleX = d3.scaleTime()
-      .domain(d3.extent(allStartEndDates, date => date.format()))
+      .domain(d3.extent(allStartEndDates))
       .range([0, widthChart]);
 
     const scaleY = d3.scaleLinear()
-      .domain([0, 100])
+      .domain([0, maxEffort])
       .range([heightChart, 0]);
 
     const axisX = d3.axisBottom(scaleX);
@@ -65,6 +78,27 @@ export const createRoadmapChart = () => {
 
     const groupAxisY = groupChart.append('g')
       .attr('class', 'rbc-group-axis-y');
+
+    groupAxisX.call(axisX);
+    groupAxisY.call(axisY);
+
+    const groupChartEllipses = groupChart.append('g');
+
+    const ellipses = groupChartEllipses.selectAll('rbc-ellipse')
+      .data(data);
+
+    ellipses.exit().remove();
+
+    const ellipsesEnter = ellipses.enter()
+      .append('ellipse')
+      .attr('class', 'rbc-ellipse')
+      .attr('cx', d => {
+        const halfDur = getHalfDuration(d.start, d.end);
+        return scaleX(moment(d.start).add(halfDur));
+      })
+      .attr('cy', scaleY(maxEffort / 2))
+      .attr('rx', 5)
+      .attr('ry', 5);
 
   };
   

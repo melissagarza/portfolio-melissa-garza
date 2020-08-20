2ddf02ce5609f9c516a9701de1cbd2f0fa60357d
Melissa Garza <melissa.garza@utexas.edu>
2020-08-20 18:01:03 -0500

    Fixed issue where the roadmap form was manipulating the chart data early (before onSubmit). Doing so by adding the hover elements from the beginning and toggling their opacity on mouseenter and mouseleave. This way React doesn't manipulate the elements when they're added and removed to and from the DOM on the hover event

--- client/src/components/roadmap/RoadmapForm.js
+++ client/src/components/roadmap/RoadmapForm.js
@@ -2,25 +2,53 @@ import React, { Fragment, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { loadRoadmapData } from '../../actions/roadmap';
 import { Form, Button } from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const RoadmapForm = () => {
 
   const dispatch = useDispatch();
 
-  const demoData = [
-    {"label":"0.5 POC","start":"07.07.2014","end":"08.10.2014","effort":25,"manpower": 1},
-    {"label":"Toolbox 1.0","start":"08.07.2014","end":"10.07.2014","effort":44,"manpower": 1},
-    {"label":"Toolbox 1.1","start":"10.03.2014","end":"12.16.2014","effort":53,"manpower": 1},
-    {"label":"Toolbox 1.2","start":"12.13.2014","end":"01.20.2015","effort":27,"manpower": 1}
-  ];
-
-  const [formData, setFormData] = useState(demoData);
+  const [formData, setFormData] = useState([]);
 
   useEffect(() => {
+    const demoData = [{
+        'label': '0.5 POC',
+        'start': new Date('07.07.2014'),
+        'end': new Date('08.10.2014'),
+        'effort': 25,
+        'manpower': 1
+      }, {
+        'label': 'Toolbox 1.0',
+        'start': new Date('08.07.2014'),
+        'end': new Date('10.07.2014'),
+        'effort': 44,
+        'manpower': 1
+      }, {
+        'label': 'Toolbox 1.1',
+        'start': new Date('10.03.2014'),
+        'end': new Date('12.16.2014'),
+        'effort': 53,
+        'manpower': 1
+      }, {
+        'label': 'Toolbox 1.2',
+        'start': new Date('12.13.2014'),
+        'end': new Date('01.20.2015'),
+        'effort': 27,
+        'manpower': 1
+      }
+    ];
     dispatch(loadRoadmapData(demoData));
-  }, [dispatch, demoData]);
+    setFormData(demoData);
+  }, [dispatch]);
 
-  const onBlur = (e, index, field) => {
+  const onChangeDate = (newDate, index, field) => {
+    let newFormData = [...formData];
+    newFormData[index][field] = newDate;
+    setFormData(newFormData);
+  };
+
+  const onChangeText = (e, index, field) => {
     let newFormData = [...formData];
     newFormData[index][field] = e.target.value;
     setFormData(newFormData);
@@ -43,36 +71,34 @@ const RoadmapForm = () => {
             <Form.Group>
               <Form.Control
                 as="input"
-                defaultValue={entry.label}
-                onBlur={e => onBlur(e, index, 'label')}
+                value={entry.label}
+                onChange={e => onChangeText(e, index, 'label')}
+              />
+            </Form.Group>
+            <Form.Group>
+              <DatePicker
+                selected={new Date(entry.start)}
+                onChange={date => onChangeDate(date, index, 'start')}
+              />
+            </Form.Group>
+            <Form.Group>
+              <DatePicker
+                selected={new Date(entry.end)}
+                onChange={date => onChangeDate(date, index, 'end')}
               />
             </Form.Group>
             <Form.Group>
               <Form.Control
                 as="input"
-                defaultValue={entry.start}
-                onBlur={e => onBlur(e, index, 'start')}
+                value={entry.effort}
+                onChange={e => onChangeText(e, index, 'effort')}
               />
             </Form.Group>
             <Form.Group>
               <Form.Control
                 as="input"
-                defaultValue={entry.end}
-                onBlur={e => onBlur(e, index, 'end')}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control
-                as="input"
-                defaultValue={entry.effort}
-                onBlur={e => onBlur(e, index, 'effort')}
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Control
-                as="input"
-                defaultValue={entry.manpower}
-                onBlur={e => onBlur(e, index, 'manpower')}
+                value={entry.manpower}
+                onChange={e => onChangeText(e, index, 'manpower')}
               />
             </Form.Group>
           </Form.Row>
